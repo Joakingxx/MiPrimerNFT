0404f2ae852ddae48af8f083c0324978ac4401a4
Joaquin Alvarez <joaquinjoby@gmail.com>
2022-01-23 14:43:05 -0300

    aa

--- hardhat.config.js
+++ hardhat.config.js
@@ -9,7 +9,7 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
     console.log(account.address);
   }
 });
-//aa
+//aaasas
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
 
