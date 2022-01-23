7db64a66026b3d8361a9da91fcadb863bbdc2b76
Joaquin Alvarez <joaquinjoby@gmail.com>
2022-01-23 00:57:04 -0300

    run.js finish

--- scripts/run.js
+++ scripts/run.js
@@ -4,3 +4,15 @@ const main = async () => {
   await nftContract.deployerd();
   console.log("Contrato desplegado en: ", nftContract.addres);
 };
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+runMain();
