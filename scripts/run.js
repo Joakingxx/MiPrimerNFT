780becd12429c3c82b9fead444310d5e36cb21c5
Joaquin Alvarez <joaquinjoby@gmail.com>
2022-01-23 01:01:05 -0300

    Se arreglo error en el llamado de la funcion deployed

--- scripts/run.js
+++ scripts/run.js
@@ -1,7 +1,7 @@
 const main = async () => {
   const nftContractFactory = await hre.ethers.getContractFactory("MiPrimerNFT");
   const nftContract = await nftContractFactory.deploy();
-  await nftContract.deployerd();
+  await nftContract.deployed();
   console.log("Contrato desplegado en: ", nftContract.addres);
 };
 
